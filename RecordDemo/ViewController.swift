cdd5094afcf5524f82c05797ecb130002a6bc420
RockerHX <rockerhx@gmail.com>
2018-07-04 14:57:10 +0800

    Initial Commit

---
+++ RecordDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordDemo
+//
+//  Created by RockerHX on 2018/7/4.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
